36c2f134173413e0256f79c49265f5bb298988e5
HuyTrinh <trnhquchuy@yahoo.com.vn>
2022-05-10 08:11:42 +0700

    finish-version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from model import *
+from numpy.random import randint
+
 
 states = ('A','B')
 
@@ -6,30 +8,39 @@ states = ('A','B')
 possible_observation = ('1','2','3','4','5','6')
 
 # The observations that we observe and feed to the model
-observations = ('1','2','3','4')
-obs4 = ('1', '3','5','6')
+observations = list(map(str,randint(1,7,100)))
+obs4 = list(map(str,randint(1,7,100)))
 
+print("Sample of 100 string sample:",observations)
 # observations
 observation_tuple = []
 observation_tuple.extend( [observations,obs4] )
-quantities_observations = [10, 20]
+quantities_observations = [100, 200]
 
 start_probability = np.matrix( '0.5 0.5')
-transition_probability = np.matrix('0.8 0.2 ;  0.3 0.7 ')
+transition_probability = np.matrix('0.2 0.8 ;  0.7 0.3 ')
 tmp_prob = 1/6
 emission_probability = np.matrix( ' {} {} {} {} {} {}  ; 0.1 0.1 0.1 0.1 0.1 0.5  '.format(tmp_prob,tmp_prob,tmp_prob,tmp_prob,tmp_prob,tmp_prob) )
+
 print(start_probability)
 print(transition_probability)
 print(emission_probability)
+
 test = HMM(states,possible_observation,start_probability,transition_probability,emission_probability)
 forw_prob = (test.forward(observations))
 forw_prob = round(forw_prob, 5)
-print(forw_prob)
+print("forward test:",forw_prob)
+
 vit_out = (test.viterbi(observations))
-print(vit_out)
+print("Result of {} sample".format(len(vit_out)))
+print("Sample:",vit_out)
+
 prob = test.log_prob(observation_tuple, quantities_observations)
 prob = round(prob, 3)
 print(prob)
 num_iter=1000
 e,t,s = test.BW(observation_tuple,num_iter,quantities_observations)
-# print(e,t,s)
+print("emission:",e)
+print("transition:",t)
+print("start prob:",s)
+
